f8db425b99e9664433eaca9f37e1eea97a8d86b8
servbotsu <tealsasuke@aol.com>
2017-03-22 07:52:39 -0400

    hound fixes

--- app/controllers/arcades_controller.rb
+++ app/controllers/arcades_controller.rb
@@ -31,7 +31,8 @@ class ArcadesController < ApplicationController
   private
 
   def arcade_params
-    params.require(:arcade).permit(:name, :street1, :street2, :city, :state, :zip)
+    params.require(:arcade).permit
+    (:name, :street1, :street2, :city, :state, :zip)
   end
 
   def authorize_user
