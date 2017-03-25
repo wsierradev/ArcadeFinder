e682b28b9a064aaa116487e42329359d00fb9b83
servbotsu <tealsasuke@aol.com>
2017-03-25 04:00:01 -0400

    hound

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  
+
   it { should have_valid(:name).when('Galaga', 'Pac-Man') }
-  it { should have_valid(:quantity).when(1,2,4,6,8) }
+  it { should have_valid(:quantity).when(1, 2, 4, 6, 8) }
   it { should have_valid(:working).when(true, false) }
 
   it { should_not have_valid(:name).when(nil) }
